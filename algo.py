1373c33e04ae7427318709087be9a65aabf9e068
Octoverse <132621685+OcstZOctoverse@users.noreply.github.com>
2025-07-22 21:35:22 -0700

    Create algo.py

---
+++ algo.py
@@ -0,0 +1,13 @@
+def arch_sort(list):
+  for i in range(len(list))
+    swap = False
+    for item in list:
+      if item > list[i]:
+        temp = list[list.index(item)]
+        list[list.index(item)] = list[i]
+        list[i] = temp
+        swap = True
+    if not swap:
+      break #I don't know if this is the fastest way to do it, go this little swap thing from someone else
+  return list
+        
