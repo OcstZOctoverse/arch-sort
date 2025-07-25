00af815b2b34b73abb7a0e0937aad3e926fd14a7
Octoverse <132621685+OcstZOctoverse@users.noreply.github.com>
2025-07-24 21:17:36 -0700

    Update algo.py
    cooking on them bug fixes, optimizations next to reduce comparisons

--- algo.py
+++ algo.py
@@ -1,21 +1,21 @@
 def arch_sort(list):
   greatest = True
   for i in range(1, len(list)):
-    if list[0] > list[i]:
+    if list[0] <= list[i]:
         greatest = False
         break
-    if greatest is True:
+    if greatest:
         for i in range(1, len(list)):
             temp = list[i]
             list[0] = list[i]
             list[i] = temp
   for i in range(len(list)):
     swap = False
-    for item in list:
-      if item > list[i]:
-        temp = list[list.index(item)]
-        list[list.index(item)] = list[i]
-        list[i] = temp
+    for j in range(i + 1, len(list)):
+      if list[i] > list[j]:
+        temp = list[i]
+        list[i] = list[j]
+        list[j] = temp
         swap = True
     if not swap:
       break #I don't know if this is the fastest way to do it, go this little swap thing from someone else
